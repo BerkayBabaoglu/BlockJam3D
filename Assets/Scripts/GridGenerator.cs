bf71ea05925261adb291b049ff4e14a787cf6c18
BerkayBabaoglu <112875768+BerkayBabaoglu@users.noreply.github.com>
2025-08-08 14:45:12 +0300

    Update

---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    public GameObject grassPrefab;
+    public int cellsX = 10;
+    public int cellsZ = 10;
+
+    private Vector3 planeSize;
+
+    private void Start()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        planeSize = mr.bounds.size;
+
+        GenerateGrid();
+    }
+
+    void GenerateGrid() {
+        float cellWidth = planeSize.x / cellsX;
+        float cellHeight = planeSize.z / cellsZ;
+
+        Debug.Log("PLANE SIZE : " + planeSize);
+        Debug.Log("CELL WIDTH : " + cellWidth);
+        Debug.Log("CELL HEIGHT: " + cellHeight);
+
+        Vector3 startPos = transform.position - new Vector3(planeSize.x / 2, 0, planeSize.z / 2);
+
+        for (int x = 0; x < cellsX; x++)
+        {
+            for (int z = 0; z < cellsZ; z++)
+            {
+                // Hücre merkezi
+                Vector3 cellPos = startPos + new Vector3(
+                    cellWidth * (x + 0.5f),
+                    0,
+                    cellHeight * (z + 0.5f)
+                );
+
+                GameObject grass = Instantiate(grassPrefab, cellPos, Quaternion.identity, transform);
+
+                // Prefab boyutunu hücreye göre ayarla
+                MeshRenderer grassRenderer = grass.GetComponent<MeshRenderer>();
+                Vector3 originalSize = grassRenderer.bounds.size;
+
+                float scaleX = cellWidth / originalSize.x;
+                float scaleZ = cellHeight / originalSize.z;
+
+                grass.transform.localScale = new Vector3(0.1884727f, 0.3907799f, 0.2211753f);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+
+        Gizmos.color = Color.black;
+
+        float cellWidth = planeSize.x / cellsX;
+        float cellHeight = planeSize.z / cellsZ;
+
+        Vector3 startPos = transform.position - new Vector3(planeSize.x / 2, 0, planeSize.z / 2);
+
+        // Dikey çizgiler
+        for (int x = 0; x <= cellsX; x++)
+        {
+            Vector3 from = startPos + new Vector3(x * cellWidth, 0, 0);
+            Vector3 to = from + new Vector3(0, 0, planeSize.z);
+            Gizmos.DrawLine(from, to);
+
+            Debug.Log("Dikey startpos:" + startPos);
+            Debug.Log("Dikey planesize.z:" + planeSize.z);
+            Debug.Log("Dikey x * cellWidth:" + x * cellWidth);
+        }
+        
+        
+        
+        // Yatay çizgiler
+        for (int z = 0; z <= cellsZ; z++)
+        {
+            Vector3 from = startPos + new Vector3(0, 0, z * cellHeight);
+            Vector3 to = from + new Vector3(planeSize.x, 0, 0);
+            Gizmos.DrawLine(from, to);
+
+            Debug.Log("Yatay startpos:" + startPos);
+            Debug.Log("Yatay planesize.x:" + planeSize.x);
+            Debug.Log("Yatay z * cellHeight:" + z * cellHeight);
+        }
+    }
+}
